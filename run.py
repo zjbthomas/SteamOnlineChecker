4fdfd0244d14c3eaa122cca8dd416feec37471e7
Junbin <zjbthomas@gmail.com>
2022-06-06 22:07:35 -0700

    fixed when nothing to commit

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ from idm import run_idm
 def _update_env(msg_steam, msg_idm):
     flag = (msg_steam is not '') or (msg_idm is not '')
 
+    flag_idm = (msg_idm is not '')
+
     msg = msg_steam + msg_idm
 
     env_file = os.getenv('GITHUB_ENV')
@@ -15,8 +17,15 @@ def _update_env(msg_steam, msg_idm):
         if (flag):
             f.write("notification=true\n")
             f.write("msg=" + msg + "\n")
+
+            if (flag_idm):
+                f.write("commit=true\n")
+            else:
+                f.write("commit=false\n")
+
         else:
             f.write("notification=false\n")
+            f.write("commit=false\n")
             
 if __name__ == "__main__":
     _update_env(run_steam(sys.argv[1], sys.argv[2]), run_idm())
