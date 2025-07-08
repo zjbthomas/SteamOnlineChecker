a191525b440626cbcca6c92308e7c3bab892c4dd
Junbin <zjbthomas@gmail.com>
2025-07-07 20:53:57 -0700

    change fetcher

--- steam_language.py
+++ steam_language.py
@@ -17,31 +17,25 @@ def _fetch_wishlist(api_key, steam_id): #Steam ID is the 64bit one
     return [item["appid"] for item in data["response"]["items"]]
 
 def _fetch_languages(steamids):
-    req_headers = {'User-Agent': 'Python script'}
-    url = "https://store.steampowered.com/api/appdetails?appids=" + steamids + "&l=english"
-    req = urllib.request.Request(url, data=None, headers=req_headers, origin_req_host=None)
-    response = urllib.request.urlopen(req)
-    content = response.read()
-    data = json.loads(content.decode('utf8'))
-    
-    return data
-
-def _check_supported_languages(wishlist, languages):
     with open(FILENAME, 'w') as f:
-        for appid in wishlist:
-            appid = str(appid)  # Ensure appid is a string for dictionary lookup
-            if appid in languages:
-                app_data = languages[appid]
-                if ("data" in app_data):
-                    supported_languages = app_data["data"]["supported_languages"]
+        for appid in steamids:
+            req_headers = {'User-Agent': 'Python script'}
+            url = "https://store.steampowered.com/api/appdetails?appids=" + appid + "&l=english"
+            req = urllib.request.Request(url, data=None, headers=req_headers, origin_req_host=None)
+            response = urllib.request.urlopen(req)
+            content = response.read()
+            data = json.loads(content.decode('utf8'))
 
-                    f.write(f"{appid},{'Chinese' in supported_languages}\n")
-    
+            if data[str(appid)]['success']:
+                app_data = data[str(appid)]
+                supported_languages = app_data["data"]["supported_languages"]
+
+                f.write(f"{appid},{'Chinese' in supported_languages}\n")
+
 def run_steam(api_key, steam_id):
     wishlist = _fetch_wishlist(api_key, steam_id)
-    languages = _fetch_languages(",".join(map(str, wishlist)))
-
-    _check_supported_languages(wishlist, languages)
+    
+    _fetch_languages(wishlist)
 
 if __name__ == "__main__":
     run_steam(sys.argv[1], sys.argv[2])
